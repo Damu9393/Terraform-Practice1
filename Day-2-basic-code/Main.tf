9be023a5f2dbecf982579a8d54c9e3d58b66a231
Damu <damodaratadimarri1992@gmail.com>
2025-02-14 14:51:04 +0530

    day-1,2

--- Day-2-basic-code/Main.tf
+++ Day-2-basic-code/Main.tf
@@ -1,1 +1,5 @@
-
+resource "aws_instance" "name" {
+  ami = var.ami_id
+  key_name = var.key
+  instance_type = var.type
+}
---
+++ Day-2-basic-code/Variables.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+  description = "inserting ec2 instance"
+  type = string
+  default = "ami-085ad6ae776d8f09c"
+}
+
+
+variable "type" {
+  type = string
+  default = "t2.micro"
+
+}
+
+
+variable "key" {
+   type = string
+   default = "MyKeyPair"
+} 
