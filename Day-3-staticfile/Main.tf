2825569157ee31d9000525c4b8f8087202ff46a6
Damu <damodaratadimarri1992@gmail.com>
2025-02-16 10:06:02 +0530

    Day-4-statefile

--- Day-3-staticfile/Main.tf
+++ Day-3-staticfile/Main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "name" {
     ami = "ami-085ad6ae776d8f09c"
     instance_type = "t2.micro"
     key_name = "MyKeyPair"
-
+    availability_zone = "us-east-1a"
           tags = {
-            name = "dev"
+            Name = "dev"
             }
    
 }
